fa7ce69c3b2f6a31a1f2176eec08ea06fadfd243
DAVIN <davin325@gmail.com>
2019-12-27 03:09:12 +0900

    첫번째 커밋

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+//components 폴더 아래의 vue파일들을 변수에 담음
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
+
+export default {
+  components: {
+    //컴포넌트 태그명 : 컴포넌트 내용
+    TodoHeader: TodoHeader,
+    TodoInput: TodoInput,
+    TodoList: TodoList,
+    TodoFooter: TodoFooter
+  }
+};
+</script>
+
+<style></style>
---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- Vue 컴포넌트 템플릿 구조는 Root에서 child 형태 -->
+  <div>header</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
