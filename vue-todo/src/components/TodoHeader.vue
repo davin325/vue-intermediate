9f6aff3cf59b556128719f5eacb159718d8b58fb
davin <davin325@gmail.com>
2019-12-27 10:01:53 +0900

    TodoHeader 컴포넌트 구현

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,11 +1,17 @@
 <template>
   <!-- Vue 컴포넌트 템플릿 구조는 Root에서 child 형태 -->
-  <div>header</div>
+  <header>
+    <h1>Todo it</h1>
+  </header>
 </template>
 
-<script>
-export default {};
-</script>
 
-<style>
+
+<style scoped>
+/* scoped : 컴포넌트 안에서만 유일한 설정 */
+h1 {
+  color: #2f3852;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
 </style>
